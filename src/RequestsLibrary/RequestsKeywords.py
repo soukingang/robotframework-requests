bfd0165a0a78654f5c6d61c7fd6ba9c425192763
soukingang <soukingang@gmail.com>
2016-05-18 16:00:47 +0800

    Update RequestsKeywords.py for python3
    Update RequestsKeywords.py for python3

--- src/RequestsLibrary/RequestsKeywords.py
+++ src/RequestsLibrary/RequestsKeywords.py
@@ -1,9 +1,10 @@
 import requests
 import json
 import vcr
-import httplib
 
-from urllib import urlencode
+from http.client import HTTPConnection
+from urllib.parse import urlencode
+from builtins import str
 
 import robot
 
@@ -359,8 +360,8 @@ class RequestsKeywords(object):
 
     def set_http_vsn(self, version):
         if version == '1.0':
-            httplib.HTTPConnection._http_vsn = 10
-            httplib.HTTPConnection._http_vsn_str = 'HTTP/1.0'
+            HTTPConnection._http_vsn = 10
+            HTTPConnection._http_vsn_str = 'HTTP/1.0'
         elif version == '1.1':
-            httplib.HTTPConnection._http_vsn = 11
+            HTTPConnection._http_vsn = 11
             httplib.HTTPConnection._http_vsn_str = 'HTTP/1.1'
